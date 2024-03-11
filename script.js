41117fe9b2b7681c0280f0b81c0ce1a71d64eb21
nmervecifci <mervenurcfc42@gmail.com>
2024-03-11 12:51:16 +0200

    end

---
+++ script.js
@@ -0,0 +1,37 @@
+const imgs=document.querySelectorAll('.header-slider ul img');
+const prev_btn=document.querySelector('.icons-left');
+const next_btn=document.querySelector('.icons-right');
+
+let n = 0;
+function changeSlide(){
+    for(let i =0;i<imgs.length;i++){
+        imgs[i].style.display='none';
+    }
+    imgs[n].style.display='block';
+}
+changeSlide();
+
+prev_btn.addEventListener('click',(e)=>{
+    if(n>0){
+        n--;
+    }else{
+        n=imgs.length-1
+    }
+    changeSlide();
+})
+next_btn.addEventListener('click',(e)=>{
+    if( n<imgs.length-1){
+        n++;
+    }else{
+        n=0
+    }
+    changeSlide();
+})
+
+const scrollContainer=document.querySelectorAll('.slides');
+for(const item of scrollContainer){
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft +=evt.deltay;
+    })
+}
